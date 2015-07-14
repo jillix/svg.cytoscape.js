573b7670121472b49d1e2976282acd52807f00be
Ionică Bizău <bizauionica@gmail.com>
2015-07-14 19:48:55 +0300

    Added some icons

--- example/js/index.js
+++ example/js/index.js
@@ -4,15 +4,18 @@ window.addEventListener("load", function () {
             "node_a": {
                 name: "A"
               , id: "node_a"
+              , icon: "&#10084;"
             }
           , "node_b": {
                 name: "B"
               , id: "node_b"
+              , icon: "&#9762;"
               , subelms: {
                     "sub_elm_1": {
                         id: "sub_elm_1"
                       , parent: "node_b"
                       , label: "Subelement 1"
+                      , icon: "&#10052;"
                     }
                 }
             }
