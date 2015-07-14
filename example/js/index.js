d1c3062272af7322d953a986594c505c5700fa15
Ionică Bizău <bizauionica@gmail.com>
2015-07-14 19:06:27 +0300

    Added a subelement

--- example/js/index.js
+++ example/js/index.js
@@ -2,8 +2,19 @@ window.addEventListener("load", function () {
     var cyto = new Cytoscape(".graph", {
         nodes: {
             "node_a": {
-                name: "A",
-                id: "node_a"
+                name: "A"
+              , id: "node_a"
+            }
+          , "node_b": {
+                name: "B"
+              , id: "node_b"
+              , subelms: {
+                    "sub_elm_1": {
+                        id: "sub_elm_1"
+                      , parent: "node_b"
+                      , label: "Subelement 1"
+                    }
+                }
             }
         }
     });
